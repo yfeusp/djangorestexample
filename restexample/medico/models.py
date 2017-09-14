435fd8b022f6803eb32db1aa5fb5d398ddd8b250
Yusdanis Feus Perez <feus2006@gmail.com>
2017-09-14 15:15:22 -0400

    Inclusión del modelo Medico y la migración correspondiente

--- restexample/medico/models.py
+++ restexample/medico/models.py
@@ -2,4 +2,25 @@
 from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+ESPECIALIDAD_CHOICES = (
+    (u'Angiología', u'Angiología'),
+    (u'Cardiología', u'Cardiología'),
+    (u'Pediatría', u'Pediatría'),
+    (u'Reumatología', u'Reumatología'),
+    (u'Urología', u'Urología')
+)
+
+
+class Medico(models.Model):
+    identificador = models.IntegerField(verbose_name='id', unique=True)
+    nombre = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=15)
+    correo = models.EmailField()
+    especialidad = models.CharField(max_length=12, choices=ESPECIALIDAD_CHOICES)
+
+    class Meta:
+        ordering = ['nombre', 'apellidos']
+
+    def __unicode__(self):
+        return '%s %s' % (self.nombre, self.apellidos)
